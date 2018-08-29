64c86dbb6a41a8baa9ccd2e0b12eba880f60430d
tmrfouad <tmrfouad@gmail.com>
2018-08-29 11:23:39 +0200

    add expensify styles

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import 'react-dates/initialize';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 const jsx = (
@@ -25,7 +26,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading ...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
